9c5d57987064848b5e1eb321da4a4fe44be65aa0
Christian <x00bit@gmail.com>
2011-07-22 15:06:58 +0300

    first commit

---
+++ botCore/moswarShop.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class moswarShop extends moswarUtils{
+    
+    public function buyItem( $itemId , $count = 1 )
+    {
+        $key = $this->getKey();
+        $type = 'normal';
+        $inputs = array(
+            'key' => $key,
+            'action' => $buy,
+            'item' => $itemId,
+            'amount' => $count,
+            'return_url' => '/',
+            'type' => $type
+        );
+        $this->sendPostData( 'shop/' , $inputs );
+    }
+    
+    protected function getKey()
+    {
+        global $html;
+        $html->load( $this->goToPage('shop/') );
+        $tmp = $html->find('span[onclick*=shopBuyItem]',0)->onclick;
+        $tmp2 = explode( "'" , $tmp );
+         return $tmp2[1];
+    }
+}
