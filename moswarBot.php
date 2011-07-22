ed4b5924e8e041073343bda379ef5500406e2545
Christian <x00bit@gmail.com>
2011-07-22 15:10:52 +0300

    Bot update 2

--- moswarBot.php
+++ moswarBot.php
@@ -5,11 +5,14 @@
 error_reporting (E_ALL | E_STRICT);
 ini_set("display_errors", 1); 
 
-include('./botCore/cUrlClass.php');
-include('./botCore/simple_html_dom.php');
-include('./botCore/moswarUtils.class.php');
-include('./botCore/moswarBot.class.php');
-include('./botCore/moswarInfo.class.php');
-include('./botCore/moswarAttack.class.php');
+include('botCore/cUrlClass.php');
+include('botCore/simple_html_dom.php');
+include('botCore/moswarUtils.class.php');
+include('botCore/moswarBot.class.php');
+include('botCore/moswarInfo.class.php');
+include('botCore/moswarAttack.class.php');
+include('botCore/moswarMetro.class.php');
+include('botCore/moswarLife.class.php');
+include('botCore/moswarShop.class.php');
 
 $html = new simple_html_dom;
