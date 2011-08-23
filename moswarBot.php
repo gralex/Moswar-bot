4a13394a480c21907483463b6511a58ade84e08b
Christian <x00bit@gmail.com>
2011-08-23 20:55:29 +0300

    Big update

--- moswarBot.php
+++ moswarBot.php
@@ -5,14 +5,24 @@
 error_reporting (E_ALL | E_STRICT);
 ini_set("display_errors", 1); 
 
+define("BOT_DEBUG" , 0);
+
 include('botCore/cUrlClass.php');
 include('botCore/simple_html_dom.php');
 include('botCore/moswarUtils.class.php');
 include('botCore/moswarBot.class.php');
 include('botCore/moswarInfo.class.php');
-include('botCore/moswarAttack.class.php');
+include('botCore/moswarWar.class.php');
 include('botCore/moswarMetro.class.php');
 include('botCore/moswarLife.class.php');
 include('botCore/moswarShop.class.php');
+include('botCore/moswarFactory.class.php');
+include('botCore/moswarPolice.class.php');
+include('botCore/moswarAutomobile.class.php');
 
 $html = new simple_html_dom;
+
+$wb = new cUrlClass;
+$wb->setCookiesPath( '' );
+$wb->setInterface( '' );
+$wb->setMainUrl( '' );
